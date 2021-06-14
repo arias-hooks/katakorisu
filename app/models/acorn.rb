a8e2674bd379c4ec3beada837ef63c3be8b66575
asakoshiro <koshikoshi7@icloud.com>
2021-06-14 14:33:23 +0900

    fix: カラム名変更に伴う変更

--- app/models/acorn.rb
+++ app/models/acorn.rb
@@ -1,6 +1,6 @@
 class Acorn < ApplicationRecord
   belongs_to :user
 
-  validates :acorn_number, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :number, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :user_id, presence: true, uniqueness: true
 end
