ae316027bb8b074930192f84aa40a96711b5006e
asakoshiro <koshikoshi7@icloud.com>
2021-06-20 09:25:23 +0900

    refactor: アプリ名をi18nにする

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def page_title(page_title = '')
-    base_title = '肩こリス'
+    base_title = t('defaults.app_name')
     page_title.empty? ? base_title : "#{page_title} - #{base_title}"
   end
 
