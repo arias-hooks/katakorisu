56963bbb5a310d7746694f38af499522f32c3495
asakoshiro <koshikoshi7@icloud.com>
2021-06-24 20:05:36 +0900

    change: fontawesomeをyarnではなくgemからインストールするように変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,6 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '@fortawesome/fontawesome-free/js/all';
 import '../css/tailwind.scss'
 
 Rails.start()
