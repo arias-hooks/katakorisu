a77eae11f2805ea5f1a36e32f35825f8243a5e91
asakoshiro <koshikoshi7@icloud.com>
2021-06-28 17:43:58 +0900

    add: mkcertの導入

--- config/puma.rb
+++ config/puma.rb
@@ -41,3 +41,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+if ENV.fetch('RAILS_ENV') { 'development' } == 'development'
+  ssl_bind '0.0.0.0', '9292', key: './localhost-key.pem', cert: './localhost.pem'
+end
