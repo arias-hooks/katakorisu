2facdaa935d068245dca4188b6f61ac6a2b2dc61
asakoshiro <koshikoshi7@icloud.com>
2021-07-12 23:04:45 +0900

    fix: purgecssを追加してデプロイできないバグの修正

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+const environment = {
   plugins: [
-    require('tailwindcss')("./app/javascript/css/tailwind.config.js"),
+    require("tailwindcss")("./app/javascript/css/tailwind.config.js"),
+    require("autoprefixer"),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -11,3 +12,19 @@ module.exports = {
     })
   ]
 }
+
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+        './app/**/*.html.slim',
+        './app/**/*.js.erb',
+        './app/helpers/**/*.rb',
+      ],
+      whitelist: ['img', 'video', ':root'],
+    })
+  )
+}
+
+module.exports = environment
