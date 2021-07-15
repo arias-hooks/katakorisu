d0bf62a1033c833994d82ed4dac8dfd262f357d1
asakoshiro <koshikoshi7@icloud.com>
2021-07-15 20:50:11 +0900

    add: アプリ説明ポップアップを追加

--- app/javascript/packs/squirrels/show.js
+++ app/javascript/packs/squirrels/show.js
@@ -2,12 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const acorn = document.getElementById('acorn');
   const closeButton = document.getElementById('close-button');
   const modal = document.getElementById('modal');
+  const description = document.getElementById('description');
+  const descriptionButton = document.getElementById('description-button');
+  const descriptionCloseButton = document.getElementById('description-close');
 
   acorn.addEventListener('click', () => {
     modal.classList.add('scale-100');
   });
 
+  descriptionButton.addEventListener('click', () => {
+    description.classList.add('scale-100');
+  });
+
   closeButton.addEventListener('click', () => {
     modal.classList.remove('scale-100');
+  });
+
+  descriptionCloseButton.addEventListener('click', () => {
+    description.classList.remove('scale-100');
   });
 })
