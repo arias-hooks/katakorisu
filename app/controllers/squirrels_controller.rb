340c990ab552ec8c58de1ec9c5c2db10775918b6
asakoshiro <koshikoshi7@icloud.com>
2021-07-18 19:39:18 +0900

    change: 友達にならなかった肩こリスは残るように変更

--- app/controllers/squirrels_controller.rb
+++ app/controllers/squirrels_controller.rb
@@ -2,13 +2,14 @@ class SquirrelsController < ApplicationController
   def show; end
 
   def update
-    acorn = current_user.acorn.number - 1
-    friends = current_user.squirrel.random_friends
-    total_friends = current_user.squirrel.total_number + friends
+    current_acorn = current_user.acorn.number - 1
+    friend_number = current_user.squirrel.random_friends
+    current_squirrel = current_user.squirrel.number - friend_number
+    total_friends = current_user.squirrel.total_number + friend_number
     ActiveRecord::Base.transaction do
-      current_user.acorn.update!(number: acorn)
-      current_user.squirrel.update!(number: 0, total_number: total_friends)
+      current_user.acorn.update!(number: current_acorn)
+      current_user.squirrel.update!(number: current_squirrel, total_number: total_friends)
     end
-    redirect_to squirrel_path(friend_num: friends), notice: t('.message', item: friends)
+    redirect_to squirrel_path(friend_num: friend_number), notice: t('.message', item: friend_number)
   end
 end
