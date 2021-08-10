895aed940baa6a8c8749f877392b21fb8b5797bf
asakoshiro <koshikoshi7@icloud.com>
2021-08-10 23:38:42 +0900

    refactor: 同じ処理をメソッドでまとめる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
+  before_action :set_liff_id, only: [:new]
 
-  def new
-    gon.liff_id = ENV['LIFF_ID']
-  end
+  def new; end
 
   def create
+    # JSからLINEのユーザーIDトークンを取得
     id_token = params[:idToken]
     channel_id = ENV['CHANNEL_ID']
     res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), { 'id_token' => id_token, 'client_id' => channel_id })
+    # LINEユーザーIDを取得
     line_user_id = JSON.parse(res.body)["sub"]
+    # ユーザー登録されているか確認
     user = User.find_by(line_user_id: line_user_id)
+    # DBにuserが保存されていればログインし、保存されていなければユーザー作成する
     if user.nil?
       user = User.create!(line_user_id: line_user_id)
       Squirrel.create!(user: user)
